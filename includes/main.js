cf28f93ebeb43863f47d57b8d272af7054f3e488
chenyafe052 <chen.yafe052@gmail.com>
2018-06-05 16:49:04 +0300

    with icons

---
+++ includes/main.js
@@ -0,0 +1,7 @@
+
+    document.getElementById("first").onclick = function(){
+    var obj = document.getElementById("first");
+    if(obj.style.display == "block")
+    obj.style.display = "none";
+    };
+    })();
