2140e123399608e5393209277b2df606f5d6bfce
chenyafe052 <chen.yafe052@gmail.com>
2018-06-06 21:45:42 +0300

    /

--- includes/main.js
+++ includes/main.js
@@ -1,30 +1,4 @@
-
-// var obj = document.getElementById('first');
-
-// window.onclick = function(event) {
-//     if (event.target == obj) {
-//         obj.style.display = "none";
-//     }
-// }
-
-// window.onload = function(){
-//     document.getElementById('first').onclick = function(){
-//         this.parentNode
-//         .removeChild(this.parentNode);
-//         return false;
-//     };
-// };
-
-
-
-
-///function fnClose() {
-   // document.getElementsByClassName("first").onclick()=function.style.visibility = "hidden";
-    // if(obj.style.display == "block")
-    
-    //}
-
-    window. onload = function() {
+window. onload = function() {
         for (i=0 ;i<4 ;++i) {
             document.getElementById("close" + i).onclick = function() {
                 this.parentNode.parentNode.style.display = "none" };
