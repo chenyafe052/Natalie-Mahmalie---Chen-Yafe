9e9e5017268354853c4a04fbe5ede80370cfcb42
chenyafe052 <chen.yafe052@gmail.com>
2018-06-06 20:38:13 +0300

    final

--- includes/main.js
+++ includes/main.js
@@ -1,7 +1,33 @@
 
-    document.getElementById("first").onclick = function(){
-    var obj = document.getElementById("first");
-    if(obj.style.display == "block")
-    obj.style.display = "none";
-    };
-    })();
+// var obj = document.getElementById('first');
+
+// window.onclick = function(event) {
+//     if (event.target == obj) {
+//         obj.style.display = "none";
+//     }
+// }
+
+// window.onload = function(){
+//     document.getElementById('first').onclick = function(){
+//         this.parentNode
+//         .removeChild(this.parentNode);
+//         return false;
+//     };
+// };
+
+
+
+
+///function fnClose() {
+   // document.getElementsByClassName("first").onclick()=function.style.visibility = "hidden";
+    // if(obj.style.display == "block")
+    
+    //}
+
+    window. onload = function() {
+        for (i=0 ;i<4 ;++i) {
+            document.getElementById("close" + i).onclick = function() {
+                this.parentNode.parentNode.style.display = "none" };
+        }
+    }
+
